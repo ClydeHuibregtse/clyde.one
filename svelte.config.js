49ce1826f473abc126dd330316fbad30701111cf
Clyde Huibregtse <clyde@huibregtse.com>
2023-11-18 23:43:35 -0600

    use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	}
 };
 
